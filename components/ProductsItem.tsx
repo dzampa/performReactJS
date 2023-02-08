a5c3c89faaca0188ab0133169072a7b29cfbff8a
Diego Zamparo <diego_zamparo@hotmail.com>
2023-02-08 07:50:18 -0300

    react virtualized

--- components/ProductsItem.tsx
+++ components/ProductsItem.tsx
@@ -1,4 +1,13 @@
-import { memo } from "react"
+import { memo, useState } from "react"
+import dynamic from "next/dynamic";
+import { AddProductToWishlistProps } from "./AddProductToWishlist";
+//import { AddProductToWishlist } from "./AddProductToWishlist";
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+    return import("./AddProductToWishlist").then(mod => mod.AddProductToWishlist)
+}, {
+    loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps {
     product: {
@@ -11,10 +20,18 @@ interface ProductItemProps {
 }
 
 function ProductItemCompontent({ product, onAddToWishlist }: ProductItemProps) {
+    const [isAddingToWishList, setIsAddingToWishlist] = useState(false)
     return (
         <div>
             {product.title} - <strong>{product.priceFormatted}</strong>
-            <button onClick={() => onAddToWishlist(product.id)}>Add to wishlist</button>
+            <button onClick={() => setIsAddingToWishlist(true)}>Adicionar aos favoritos</button>
+
+            {isAddingToWishList === true && (
+                <AddProductToWishlist
+                    onAddToWishList={() => onAddToWishlist(product.id)}
+                    onRequestClose={() => setIsAddingToWishlist(false)}
+                />
+            )}
         </div>
     )
 }
